5aa3aa14f20c4229eb3c804f727d2054dd90031b
Tarun Kumar <14itarunkumar@gmail.com>
2024-10-02 23:15:20 +0530

    Controller & Router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from cors;
+import cors from "cors"
 import cookieParser from "cookie-parser";
 
 const app = express()
@@ -14,5 +14,16 @@ app.use(express.json({limit : limit}))
 app.use(express.urlencoded({extended : true , limit : limit}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+//routes
+
+import userRouter from './routes/user.routers.js'
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
 
 export{ app }
